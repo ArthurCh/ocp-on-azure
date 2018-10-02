7a6749e6ffddfadf99839bc696890485756c7da5
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-10-02 14:12:26 -0700

    Updated terraform scripts

--- terraform-deploy/apply.sh
+++ terraform-deploy/apply.sh
@@ -6,8 +6,7 @@ export ARM_CLIENT_SECRET=$3
 export ARM_SUBSCRIPTION_ID=$4
 export ARM_TENANT_ID=$5
 export ARM_ACCESS_KEY=$6
-echo "ARM_ACCESS_KEY=$ARM_ACCESS_KEY"
-export SSH_PUB_KEY=$7
+echo "SSH_PUBLIC_KEY=$7"
 
 cd $1/terraform-deploy/azurerm
 echo "Switched directory to => $PWD"
