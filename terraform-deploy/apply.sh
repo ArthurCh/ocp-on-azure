00521c4b76ef72338e56626be56f5e4e982f75d0
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-10-02 16:37:59 -0700

    Updated terraform scripts

--- terraform-deploy/apply.sh
+++ terraform-deploy/apply.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "************* Assigning Azure credentials to env. variables ..."
-## execute terrafotm build and sendout to packer-build-output
 export ARM_CLIENT_ID=$1
 export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
@@ -8,10 +7,10 @@ export ARM_TENANT_ID=$4
 export ARM_ACCESS_KEY=$5
 echo "SSH_PUBLIC_KEY=$6"
 
-echo "Current working directory to => $PWD"
+echo "Current working directory => $PWD"
 echo "Dir list => "
 ls -l
 
-echo "Executing Terraform Init ..."
+echo "Executing Terraform Apply ..."
 terraform apply -auto-approve -var ssh_key="$6"
 echo "Done"
--- terraform-deploy/destroy.sh
+++ terraform-deploy/destroy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "************* Assigning Azure credentials to env. variables ..."
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_SUBSCRIPTION_ID=$3
+export ARM_TENANT_ID=$4
+export ARM_ACCESS_KEY=$5
 echo "Current working directory => $PWD"
 
 echo "Executing Terraform Destroy ..."
